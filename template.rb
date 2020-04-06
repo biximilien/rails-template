3e88d5e14f110d073024bfb9c86cebdcef119ed5
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2020-04-06 15:57:05 -0400

    feat: Add `template.rb`
    Add `template.rb` file with Rails template instructions to generate a
    new project.

---
+++ template.rb
@@ -0,0 +1,30 @@
+
+# Authentication
+gem 'devise'
+
+# Authorization
+gem 'cancancan'
+
+# Foreman
+gem 'foreman'
+
+# HAML
+gem 'haml'
+
+# RSpec
+gem 'rspec-rails', '~> 4.0.0'
+
+
+# Setup action mailer default url options (needed by Devise)
+environment 'config.action_mailer.default_url_options = { host: "localhost", port: 5000 }', env: 'development'
+
+# Setup database
+rails_command "db:create"
+rails_command "db:migrate"
+
+# Add root route
+root to: 'pages#home'
+
+git :init
+git add: '.'
+git commit: '-a -m "feat: Initial commit\n\nInitial commit with Ruby on Rails application template from biximilien/rails-template 🚀"'
