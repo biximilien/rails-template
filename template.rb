fd5a2fe1f5ab50a779aa170c9317f51450ffc069
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2020-04-06 16:33:13 -0400

    feat: Add `devise` install and `yarn` dependencies to `template.rb`
    Add `devise` install instructions to the template file.
    Add `bulma` and `fontawesome-free` instructions.
    Add `Procfile` for `foreman` and Heroku.

--- template.rb
+++ template.rb
@@ -2,6 +2,15 @@
 # Authentication
 gem 'devise'
 
+# Install devise
+generate 'devise:install'
+
+# Setup action mailer default url options (needed by Devise)
+environment 'config.action_mailer.default_url_options = { host: "localhost", port: 5000 }', env: 'development'
+
+# Setup User
+generate 'devise User'
+
 # Authorization
 gem 'cancancan'
 
@@ -14,17 +23,30 @@ gem 'haml'
 # RSpec
 gem 'rspec-rails', '~> 4.0.0'
 
+# bulma CSS framework
+run "yarn add bulma"
 
-# Setup action mailer default url options (needed by Devise)
-environment 'config.action_mailer.default_url_options = { host: "localhost", port: 5000 }', env: 'development'
+# FontAwesome
+run "yarn add @fortawesome/fontawesome-free"
 
 # Setup database
 rails_command "db:create"
 rails_command "db:migrate"
 
+# Generate a static pages controller with a home page
+generate(:controller, "pages home")
+
 # Add root route
 route "root to: 'pages#home'"
 
-git :init
-git add: '.'
-git commit: '-a -m "feat: Initial commit\n\nInitial commit with Ruby on Rails application template from biximilien/rails-template 🚀"'
+# Generate a Procfile for `foreman` and Heroku
+file 'Procfile', <<~PROCFILE
+  web: bundle exec puma -C config/puma.rb
+PROCFILE
+
+# Setup Git and first commit
+after_bundle do
+  git :init
+  git add: '.'
+  git commit: '-a -m "feat: Initial commit" -m "Initial commit with Ruby on Rails application template from biximilien/rails-template 🚀"'
+end
